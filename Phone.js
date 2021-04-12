1d6b065d4191833ad6d44a256effd6430ba94f28
jainilsoni1706 <75731365+jainilsoni1706@users.noreply.github.com>
2021-04-12 22:38:07 +0530

    Add files via upload

---
+++ Phone.js
@@ -0,0 +1,20 @@
+function rtt(){
+    document.querySelector('.phone').style.transform = 'rotate(90deg)';
+}
+function red(){
+    document.querySelector('.phone').style.background = '#FF4136';
+    document.querySelector('#innvbtn').style.background = 'red';
+    document.querySelector('.branding').style.background = '#FF4136';
+}
+
+function green(){
+    document.querySelector('.phone').style.background = 'green';
+   document.querySelector('.branding').style.background = 'green';
+   document.querySelector('#innvbtn').style.background = 'green';
+}
+
+function blue(){
+    document.querySelector('.phone').style.background = 'blue';
+    document.querySelector('#innvbtn').style.background = 'blue';
+    document.querySelector('.branding').style.background = 'blue';
+}
